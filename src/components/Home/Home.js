d4de18710c3fa9b9e1c3f1550e08681405f398ee
Safinxr <safinxr@gmail.com>
2022-04-04 00:22:45 +0600

    component added

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import pcImg from "../../Assets/pcImg.png";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Home;
