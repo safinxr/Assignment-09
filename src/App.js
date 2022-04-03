d690a9f075e5f630171dea16f7eb106e3f133315
Safinxr <safinxr@gmail.com>
2022-04-04 02:28:13 +0600

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import NotFound from "./components/NotFound/NotFound";
+import Reviews from "./components/Reviews/Reviews";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Blogs from "./components/Blogs/Blogs";
+import About from "./components/About/About";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/reviews" element={<Reviews></Reviews>}></Route>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/gg" element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,31 @@
 import React from "react";
-import pcImg from "../../Assets/pcImg.png";
+import { Col, Container, Row } from "react-bootstrap";
 import "./Home.css";
+import pcImg from "../../Assets/pcImg.png";
 
 const Home = () => {
   return (
     <div>
-      
+      <Container>
+        <Row>
+          <Col xs={12} md={8}>
+            <h1 className="m-0 display-4 fw-bold ">Your Next Gaming PC</h1>
+            <h1 className="m-0 display-4 fw-bold text-primary">
+              RTX 3090 DDr6 12gb
+            </h1>
+            <p className="justify my-4 me-5">A powerful gaming PC is a huge investment but it pays off in a variety of ways. In addition to experiencing games at blazing fast frame rates and ultra-high resolution, you can also use your premium-priced PC for graphic design, animation, streaming movies in glorious 4K on your TV or simply to get work done. <br/> With next-gen consoles in the market we should point out that while the PS5 and Xbox Series X deliver plenty of power, they're still no match for one of the best gaming desktops. If you want fast-loading SSDs, gorgeous ray tracing, massive quantities of RAM, 4K resolution and 60+ fps frame rate, you don't have to wait for these consoles to be back in stock; you can have the best gaming desktops right now.</p>
+            <a className="btn btn-primary px-5 py-3" target='_blank' href="https://www.pcmag.com/picks/the-best-gaming-desktops"> More info</a>
+          </Col>
+          <Col xs={12} md={4}>
+            <img className="w-100" src={pcImg} alt="" />
+          </Col>
+        </Row>
+        <div className="my-5 text-center">
+          <h1>Coustomer Revieus ( )</h1>
+          
+        </div>
+      </Container>
+
     </div>
   );
 };
