1256aa12708042c8c84ff5d54a6e065cfe1375c0
Safinxr <safinxr@gmail.com>
2022-04-04 15:43:45 +0600

    4o4 added

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
         <Route path="/blogs" element={<Blogs></Blogs>}></Route>
         <Route path="/about" element={<About></About>}></Route>
-        <Route path="/gg" element={<NotFound></NotFound>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const NotFound = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="d-flex justify-content-center my-5">
+      <img 
+        src="https://www.pngkit.com/png/full/930-9306658_404-not-found.png"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default NotFound;
