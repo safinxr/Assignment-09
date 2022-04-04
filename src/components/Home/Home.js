567a452dd3a18e04b83919f5f132e05222db8fa9
Safinxr <safinxr@gmail.com>
2022-04-04 14:22:33 +0600

    home reviews area added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,9 +4,11 @@ import "./Home.css";
 import pcImg from "../../Assets/pcImg.png";
 import useReviews from "../../coustomHooks/useRevieus";
 import ReviewsCart from "../ReviewsCart/ReviewsCart";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const [reviews, setReviews] = useReviews();
+  const limitedReview = reviews.slice(0, 3);
   return (
     <div>
       <Container>
@@ -16,22 +18,41 @@ const Home = () => {
             <h1 className="m-0 display-4 fw-bold text-primary">
               RTX 3090 DDr6 12gb
             </h1>
-            <p className="justify my-4 me-5">A powerful gaming PC is a huge investment but it pays off in a variety of ways. In addition to experiencing games at blazing fast frame rates and ultra-high resolution, you can also use your premium-priced PC for graphic design, animation, streaming movies in glorious 4K on your TV or simply to get work done. <br/> With next-gen consoles in the market we should point out that while the PS5 and Xbox Series X deliver plenty of power, they're still no match for one of the best gaming desktops. If you want fast-loading SSDs, gorgeous ray tracing, massive quantities of RAM, 4K resolution and 60+ fps frame rate, you don't have to wait for these consoles to be back in stock; you can have the best gaming desktops right now.</p>
-            <a target='_blank' className="btn btn-primary px-5 py-3" href="https://www.pcmag.com/picks/the-best-gaming-desktops"> More info</a>
+            <p className="justify my-4 me-5">
+              A powerful gaming PC is a huge investment but it pays off in a
+              variety of ways. In addition to experiencing games at blazing fast
+              frame rates and ultra-high resolution, you can also use your
+              premium-priced PC for graphic design, animation, streaming movies
+              in glorious 4K on your TV or simply to get work done. <br /> With
+              next-gen consoles in the market we should point out that while the
+              PS5 and Xbox Series X deliver plenty of power, they're still no
+              match for one of the best gaming desktops. If you want
+              fast-loading SSDs.
+            </p>
+            <a
+              target="_blank"
+              className="btn btn-primary px-5 py-3"
+              href="https://www.pcmag.com/picks/the-best-gaming-desktops"
+              rel="noreferrer"
+            >
+              {" "}
+              More info
+            </a>
           </Col>
           <Col xs={12} md={4}>
             <img className="w-100" src={pcImg} alt="" />
           </Col>
         </Row>
-        <div className="my-5 w-50% ">
-          <h1 className="text-center">Coustomer Revieus ( {reviews.length} )</h1> 
-          {
-            reviews.map(data => <ReviewsCart key={data.id} data={data}></ReviewsCart> )
-          }
-          
+        <div className="my-5 mx-auto shadow-lg p-4 rounded w-100">
+          <h1 className="text-center">
+            Customers Reviews ( {reviews.length} )
+          </h1>
+          {limitedReview.map((data) => (
+            <ReviewsCart key={data.id} data={data}></ReviewsCart>
+          ))}
+          <Link className="btn btn-primary px-5 py-2" to='/reviews'>More Reviews</Link>
         </div>
       </Container>
-
     </div>
   );
 };
--- src/components/ReviewsCart/ReviewsCart.js
+++ src/components/ReviewsCart/ReviewsCart.js
@@ -3,14 +3,14 @@ import './ReviewsCart.css'
 const ReviewsCart = ({data}) => {
     const {name, picture, rating, review} =data
     return (
-        <div className='review-cart d-flex'>
+        <div className='review-cart d-flex align-item-center my-3 shadow-lg p-3 rounded'>
             <div>
                 <img src={picture} alt="" />
             </div>
-            <div className='ms-3'>
-                <h3>{name}</h3>
-                <p>{review}</p>
-                <p>Rating: {rating} star</p>
+            <div className='ms-4 '>
+                <h5 className='m-0'>{name}</h5>
+                <p  className='m-0'>{review}</p>
+                <p  className='m-0'>Rating: {rating} star</p>
             </div>
         </div>
     );
