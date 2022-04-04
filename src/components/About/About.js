88a1f962a91df447dc33366bc5b51c0971ed1147
Safinxr <safinxr@gmail.com>
2022-04-04 18:00:42 +0600

    about section added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            
+        <div className='container'>
+            <h1 className='text-primary text-center my-5'><u>About</u></h1>
+            <h4>Website info</h4>
+            <ul>
+                <li>This website is about gaming pc.</li>
+                <li>it's have 5 different section.</li>
+                <li>On Dashboard section its have 2 chart that shows sell and difference between Investment and revenue.</li>
+                <li>On review section you can see user review.</li>
+            </ul>
         </div>
     );
 };
